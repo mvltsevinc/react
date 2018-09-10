93bfbeba8c706d3e645c26306aba79e911b0d4c7
LAPTOP-516N2IE7\mvltsevinc <sevincmevlut@gmail.com>
2018-09-11 00:01:11 +0300

    Total Price in modal

--- burgerProjesi/burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
+++ burgerProjesi/burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -17,6 +17,7 @@ const orderSummary = props => {
       <h3>Your Order</h3>
       <p>A delicious burger with the following ingredients:</p>
       <ul>{ingredientSummary}</ul>
+      <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
       <p>Continue to Checkout?</p>
       <Button  btnType="Danger" clicked={props.purchaseCanceled}>CANCEL</Button>
       <Button  btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
--- burgerProjesi/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burgerProjesi/burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -100,6 +100,7 @@ class BurgerBuilder extends Component {
         >
           <OrderSummary
             ingredients={this.state.ingredients}
+            price={this.state.totalPrice}
             purchaseCanceled={this.purchaseCancelHandler}
             purchaseContinued={this.purchaseContinueHandler}
           />
