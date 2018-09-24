cf15917698320c0bab0cc1da5bfffa3ac0a51c63
LAPTOP-516N2IE7\mvltsevinc <sevincmevlut@gmail.com>
2018-09-24 23:48:19 +0300

    141. Prop Type Validation

---
+++ burgerProjesi/burger-builder/src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -0,0 +1,11 @@
+import React from "react";
+import classes from "./DrawerToggle.css";
+
+const drawerToggle = props => (
+  <div className={classes.DrawerToggle} onClick={props.clicked}>
+    <div />
+    <div />
+    <div />
+  </div>
+);
+export default drawerToggle;
