41830b6286db6167faeefc951c85d1159aa75bd2
LAPTOP-516N2IE7\mvltsevinc <sevincmevlut@gmail.com>
2018-11-05 00:19:36 +0300

    routing checkout

---
+++ burgerProjesi/burger-builder/src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Burger from "../../Burger/Burger";
+import Button from "../../UI/Button/Button";
+import classes from "./CheckoutSummary.css";
+
+const checkoutSummary = props => {
+  return (
+    <div className={classes.CheckoutSummary}>
+      <h1>We hope it tastes well!!</h1>
+      <div style={{ width: "100%", margin: "auto" }}>
+        <Burger ingredients={props.ingredients} />
+      </div>
+      <Button btnType="Danger">Cancel</Button>
+      <Button btnType="Success">Continue</Button>
+    </div>
+  );
+};
+
+export default checkoutSummary;
